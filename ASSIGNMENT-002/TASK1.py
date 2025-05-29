2c57b25938d4e214e8873f44ce3ce4ea2291560d
Yashknl <richonoob51@gmail.com>
2025-05-29 15:24:53 +0530

    Add files via upload

---
+++ ASSIGNMENT-002/TASK1.py
@@ -0,0 +1,13 @@
+#               ASSIGNMENT-002
+
+#         MODULE-3 (CONT. STRUCTURES IN PYTHON)
+
+#TASK ----> 1 {CHECK IF A NUMBER IS EVEN OR ODD}
+num = int(input("ENTER A NUMBER: "))
+if num % 2 == 0:
+    print(f"THE NUMBER {num} IS EVEN")
+else:
+    print(f"THE NUMBER {num} IS ODD")
+
+
+
---
+++ ASSIGNMENT-002/TASK2.py
@@ -0,0 +1,14 @@
+
+#TASK --->2 (SUM OF INTEGERS FROM 1 TO 50 USING A LOOP
+
+
+
+#RANGE
+for i in range(1,51):
+ print(i)
+
+
+
+
+a = sum(range(1,51))
+print(f"THE SUM OF NUMBERS FROM 1 TO 50 IS: {a}")
